55dcacbde266b31b755d483cfb7053d92a0a5885
szczepaj <jakub.szczepanik@vimn.com>
2023-02-08 21:02:34 +0100

    add scratchfile

---
+++ src/components/lesson6/scratch.js
@@ -0,0 +1,43 @@
+const url = 'https://something.com/api'
+
+const fetchData = async () => {
+    const response = await fetch(url)
+    const responseBody = await response.json()
+
+    try {
+
+    } catch () {
+
+    }
+
+    return responseBody
+}
+
+const fetchDataPromise = () => {
+    return fetch(url).().()
+}
+
+const body = await fetchData()
+
+const bodyPromise = fetchDataPromise()
+    .then(res => res.json)
+    .then(() => console.log('do something'))
+    .catch()
+
+
+fetch(url).then((response) => {
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error('Something went wrong');
+})
+    .then((responseJson) => {
+        // Do something with the response
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+
+// ASYNC/AWAIT
+// 1. Inny scope - wewnatrz funkcji / Promise -> wewnatrz callbacka
+// 2. Wiecej zastosowan dla async/await - testy/manipulacja danymi
